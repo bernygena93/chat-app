07eb9f38a0049418ba6783e4a7266901c09e842e
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-22 11:12:11 -0300

    added socket-io.client

--- chat-react/src/components/input/InputMessage.tsx
+++ chat-react/src/components/input/InputMessage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import SendIcon from "@mui/icons-material/Send";
 import styles from "./inputMessage.module.scss";
+import socket from "../../socket";
 
 function InputMessage() {
+  socket.emit("conectado", "hola");
+
   return (
     <div className={styles.container}>
       <div className={styles.inputContainer}>
